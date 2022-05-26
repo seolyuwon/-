548c0bf81e4bc742ccece0cd07e982132365e35e
seolyuwon <94011676+seolyuwon@users.noreply.github.com>
2022-05-27 00:16:22 +0900

    first vision

---
+++ 실생활에 사용되는 예시/main.js
@@ -0,0 +1,59 @@
+function show보안관제사() {
+  document.getElementById("fig").src = "img/정보보안 관제사.png";
+  document.getElementById("desc").innerHTML =
+    "방화벽, IPS등 침입 방지 혹은 게이트의 로그와 트래픽을 관제하며 침해 위협이 있거나 비정상적인 트래픽이 있을 경우 이를 대처하는 역할을 한다..";
+}
+
+function show정보보호제품개발자() {
+  document.getElementById("fig").src = "img/Ahnlap.jpg";
+  document.getElementById("desc").innerHTML =
+    "v3, 알약같은 정보보안에 사용되는 제품(프로그램)들을 개발하는 직종입니다.";
+}
+
+function show악성코드분석전문가() {
+  document.getElementById("fig").src = "img/악성코드분석전문가.jpeg";
+  document.getElementById("desc").innerHTML =
+    "침입한 악성코드를 분석하거나 다른 악성코드의 유형을 분석하여 이를 막을 방안을 찾거나 이를 차단하는 프로그램을 개발한다.";
+}
+
+function show침해사고대응전문가() {
+  document.getElementById("fig").src = "img/침해사고대응전문가.jpg";
+  document.getElementById("desc").innerHTML =
+    "보안 사고가 발생했을 때 피해규모를 최소화하기 위해 사고를 보고하고 시스템을 복구하고 예방전략을 수립하는 일을 한다.";
+}
+
+function show모의해킹전문가() {
+  document.getElementById("fig").src = "img/모의해킹전문가.jpeg";
+  document.getElementById("desc").innerHTML =
+    "조직의 보안망을 모의로 해킹하여 이에대한 취약점을 찾는 일을 한다. 취약점을 미리 찾아 이를 대응할 수 있게 해주는 일을 한다.";
+}
+
+function show보안관리자() {
+  document.getElementById("fig").src = "img/보안관리사.jpg";
+  document.getElementById("desc").innerHTML =
+    "조직적인 관점에서 보안이라는 목적을 달성하기 위하여 보안과 관련된 정책-관리체계-시스템 구축-운영(관리)업무를 실제적으로 수핸하는 전문가.";
+}
+
+function show정보보안정책전략전문가() {
+  document.getElementById("fig").src = "img/정보보안정책.jpg";
+  document.getElementById("desc").innerHTML =
+    "보안전략을 총괄적-통합적으로 수립하고 운영하며 조정하는 전문가.";
+}
+
+function show정보보호컨설턴트() {
+  document.getElementById("fig").src = "img/정보보안컨설턴트.jpeg";
+  document.getElementById("desc").innerHTML =
+    "고객의 정보자산과 비즈니스 프로세스에 따른 위협 및 취약점을 분석하여 보안수준을 파악하고, 요구수준에 맞는 통합적인 보안 해결책을 설계하는 전문가.";
+}
+
+function show디지털포렌식전문가() {
+  document.getElementById("fig").src = "img/디지털포렌식.jpeg";
+  document.getElementById("desc").innerHTML =
+    "정보 자산을 위협하여 보안사고를 발생 시키는 요인에 대하여 증거를 수집하여 복구하고 추적하는 활동을 하는 전문가.";
+}
+
+function hide() {
+  document.getElementById("fig").src = "";
+  document.getElementById("desc").innerHTML = "";
+}
+console.log(예시);
